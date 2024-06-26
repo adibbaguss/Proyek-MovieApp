c253cababc379a8f2b02efadcb557aef38a3673a
ozandilah <ozan.it212@gmail.com>
2024-06-26 08:46:12 +0700

    tab navigate screen

---
+++ src/app/_layout.tsx
@@ -0,0 +1,36 @@
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+  useFonts,
+} from "@expo-google-fonts/inter";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { SplashScreen } from "expo-router";
+import { useEffect } from "react";
+import App from "./index";
+
+SplashScreen.preventAutoHideAsync();
+export default function RootLayout() {
+  const [fontLoaded, fontError] = useFonts({
+    Inter: Inter_400Regular,
+    InterSemi: Inter_600SemiBold,
+    InterBlack: Inter_900Black,
+    InterBold: Inter_700Bold,
+  });
+  useEffect(() => {
+    if (fontLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded, fontError]);
+  if (!fontLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer independent={true}>
+      <App />
+    </NavigationContainer>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,42 @@
+import { Feather } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/Home";
+import SearchScreen from "./screens/Search";
+import FavoriteScreen from "./screens/Favorite";
+export default function App() {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="home" size={28} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="search" size={28} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="heart" size={28} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
