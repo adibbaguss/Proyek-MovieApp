b3cbf32085ceca25f6b201162aa798da124ec83e
ozandilah <ozan.it212@gmail.com>
2024-06-27 03:08:29 +0700

    selesai sampai detail movie

--- src/app/components/search/CategorySearch.tsx
+++ src/app/components/search/CategorySearch.tsx
@@ -1,122 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, ScrollView, ActivityIndicator } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { API_ACCESS_TOKEN } from '@env'
-import { Movie, MovieGenre } from '../../types/app';
-import MovieItem from '../movies/MovieItem';
-import { FlatList } from 'react-native-gesture-handler';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { API_ACCESS_TOKEN } from "@env";
+import MovieItem from "../movies/MovieItem";
+import { FlatList } from "react-native-gesture-handler";
+import { Movie, MovieGenre } from "services/data-types";
 
 const coverImageSize = {
-    backdrop: {
-        width: 280,
-        height: 160,
-    },
-    poster: {
-        width: 100,
-        height: 160,
-    },
-}
+  backdrop: {
+    width: 280,
+    height: 160,
+  },
+  poster: {
+    width: 100,
+    height: 160,
+  },
+};
 
 const CategorySearch = (): JSX.Element => {
-    const [genres, setGenres] = useState<MovieGenre[]>([]);
-    const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
-    const [movies, setMovies] = useState<Movie[]>([])
-    const [isLoading, setIsLoading] = useState(false)
-    const navigation = useNavigation();
+  const [genres, setGenres] = useState<MovieGenre[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        // Fetch genres from TMDB
-        const fetchGenres = () => {
+  useEffect(() => {
+    // Fetch genres from TMDB
+    const fetchGenres = () => {
+      const url = "https://api.themoviedb.org/3/genre/movie/list";
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+        },
+      };
 
-            const url = 'https://api.themoviedb.org/3/genre/movie/list';
-            const options = {
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: `Bearer ${API_ACCESS_TOKEN}`,
-                },
-            };
+      fetch(url, options)
+        .then((response) => response.json())
+        .then((response) => {
+          setGenres(response.genres);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchGenres();
+  }, []);
 
-            fetch(url, options)
-                .then((response) => response.json())
-                .then((response) => {
-                    setGenres(response.genres);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
+  useEffect(() => {
+    // Fetch movies by selected genre from TMDB
+    if (selectedGenre) {
+      const fetchMovies = () => {
+        setIsLoading(true);
+        const url = `https://api.themoviedb.org/3/discover/movie?with_genres=${selectedGenre}`;
+        const options = {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+          },
         };
-        fetchGenres();
-    }, []);
 
-    useEffect(() => {
-        // Fetch movies by selected genre from TMDB
-        if (selectedGenre) {
-            const fetchMovies = () => {
-                setIsLoading(true);
-                const url = `https://api.themoviedb.org/3/discover/movie?with_genres=${selectedGenre}`;
-                const options = {
-                    method: 'GET',
-                    headers: {
-                        accept: 'application/json',
-                        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
-                    },
-                };
+        fetch(url, options)
+          .then((response) => response.json())
+          .then((response) => {
+            setIsLoading(false);
+            setMovies(response.results);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+      fetchMovies();
+    }
+  }, [selectedGenre]);
 
-                fetch(url, options)
-                    .then((response) => response.json())
-                    .then((response) => {
-                        setIsLoading(false);
-                        setMovies(response.results);
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    });
-            };
-            fetchMovies();
-        }
-    }, [selectedGenre]);
-
-    return (
-        <View>
-            <ScrollView
-                style={{ marginTop: 10 }}
-                horizontal
-            >
-                {genres.map((genre) => (
-                    <TouchableOpacity
-                        key={genre.id}
-                        onPress={() => setSelectedGenre(genre.id)}
-                        style={{
-                            backgroundColor: selectedGenre === genre.id ? 'blue' : 'grey',
-                            padding: 10,
-                            margin: 5,
-                            borderRadius: 5,
-
-                        }}
-                    >
-                        <Text>{genre.name}</Text>
-                    </TouchableOpacity>
-                ))}
-            </ScrollView>
-            {isLoading ? (
-                <ActivityIndicator size="large" />
-            ) : (
-                <FlatList
-                    data={movies}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({ item }) => (
-                        <MovieItem
-                            movie={item}
-                            coverType="poster"
-                            size={coverImageSize.poster}
-                        />
-                    )}
-                    numColumns={3}
-                />
-            )}
-        </View>
-    );
+  return (
+    <View>
+      <ScrollView style={{ marginTop: 10 }} horizontal>
+        {genres.map((genre) => (
+          <TouchableOpacity
+            key={genre.id}
+            onPress={() => setSelectedGenre(genre.id)}
+            style={{
+              backgroundColor: selectedGenre === genre.id ? "blue" : "grey",
+              padding: 10,
+              margin: 5,
+              borderRadius: 5,
+            }}
+          >
+            <Text>{genre.name}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+      {isLoading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <FlatList
+          data={movies}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <MovieItem
+              movie={item}
+              coverType="poster"
+              size={coverImageSize.poster}
+            />
+          )}
+          numColumns={3}
+        />
+      )}
+    </View>
+  );
 };
 
 export default CategorySearch;
--- src/app/screens/Home/index.tsx
+++ src/app/screens/Home/index.tsx
@@ -1,47 +1,58 @@
-import React, { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { getMovieCategory } from "services/movie";
+import MovieList from "@/app/components/movies/MovieList";
+import React from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { MovieListProps } from "services/data-types";
 
 type PropsHomeScreen = {
   navigation: any;
 };
 
+const movieLists: MovieListProps[] = [
+  {
+    title: "Now Playing in Theater",
+    path: "movie/now_playing?language=en-US&page=1",
+    coverType: "backdrop",
+  },
+  {
+    title: "Upcoming Movies",
+    path: "movie/upcoming?language=en-US&page=1",
+    coverType: "poster",
+  },
+  {
+    title: "Top Rated Movies",
+    path: "movie/top_rated?language=en-US&page=1",
+    coverType: "poster",
+  },
+  {
+    title: "Popular Movies",
+    path: "movie/popular?language=en-US&page=1",
+    coverType: "poster",
+  },
+];
 const HomeScreen = ({ navigation }: PropsHomeScreen) => {
-  const [result, setResult] = useState();
-  const [isDataLoaded, setIsDataLoaded] = useState(false);
-
-  const fetchData = async () => {
-    try {
-      const response = await getMovieCategory();
-      if (!response.error) {
-        setResult(response.data);
-        setIsDataLoaded(true);
-        console.log(response.data);
-      } else {
-        console.error(response.message);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <View style={styles.container}>
-      <Button title="Tampilkan Detail Film" onPress={fetchData} />
-      {isDataLoaded && (
-        <Text style={styles.marginVertical20}>Data berhasil diambil!</Text>
-      )}
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {movieLists.map((movieList) => (
+          <MovieList
+            title={movieList.title}
+            path={movieList.path}
+            coverType={movieList.coverType}
+            key={movieList.title}
+          />
+        ))}
+        <StatusBar translucent={false} />
+      </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop: StatusBar.currentHeight ?? 32,
     alignItems: "center",
     justifyContent: "center",
-  },
-  marginVertical20: {
-    marginVertical: 20,
+    rowGap: 16,
   },
 });
 
