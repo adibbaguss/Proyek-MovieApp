cd9dc102cd2da830fc766638f56ccc554a6ce231
ozandilah <ozan.it212@gmail.com>
2024-06-26 10:39:39 +0700

    env

--- src/app/screens/Home/index.tsx
+++ src/app/screens/Home/index.tsx
@@ -1,21 +1,36 @@
-import React from "react";
-import { Button, StyleSheet, View } from "react-native";
+import axios from "axios";
+import React, { useState } from "react";
+import { Button, StyleSheet, View, Text } from "react-native";
 
 type PropsHomeScreen = {
   navigation: any;
 };
+
 const HomeScreen = ({ navigation }: PropsHomeScreen) => {
+  const [result, setResult] = useState();
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+  const fetchData = () => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/popular`, {
+        headers: {
+          Authorization: `Bearer ${process.env.EXPO_PUBLIC_API_ACCESS_TOKEN}`,
+          Accept: "application/json",
+        },
+      })
+      .then((res) => {
+        setResult(res.data);
+        setIsDataLoaded(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <View style={styles.container}>
-      <Button
-        title="Go to Movie Detail"
-        onPress={() =>
-          navigation.navigate("MovieDetail", {
-            name: "Ninja Ken",
-            language: "React Native",
-          })
-        }
-      />
+      <Button title="Tampilkan Detail Film" onPress={fetchData} />
+      {isDataLoaded && (
+        <Text style={styles.marginVertical20}>Data berhasil diambil!</Text>
+      )}
     </View>
   );
 };
