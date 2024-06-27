5467d901df2729e491d3c068796390f8ef012bb6
bagues33 <bagustofan@yahoo.com>
2024-06-27 13:11:26 +0700

    ubah function fetchGenres, fetchMovies, dan ubah tampilan pada halaman search

--- src/app/components/movies/MovieItem.tsx
+++ src/app/components/movies/MovieItem.tsx
@@ -13,6 +13,10 @@ const MovieItem = ({ movie, size, coverType }: MovieItemProps) => {
   });
   return (
     <TouchableOpacity
+      style={{
+        padding: 4,
+        borderRadius: 8,
+      }}
       onPress={() => {
         navigation.dispatch(pushAction);
       }}
@@ -44,6 +48,7 @@ const MovieItem = ({ movie, size, coverType }: MovieItemProps) => {
 const styles = StyleSheet.create({
   backgroundImage: {
     marginRight: 4,
+    borderRadius: 8,
   },
   backgroundImageStyle: {
     borderRadius: 8,
